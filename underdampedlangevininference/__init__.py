4b6006214a6739b65dfea1a29749cddc3e593c90
William Davis <38541020+williamjsdavis@users.noreply.github.com>
2022-01-25 12:00:31 -0800

    Import order

--- underdampedlangevininference/__init__.py
+++ underdampedlangevininference/__init__.py
@@ -1,6 +1,6 @@
 from .ULI_langevin import UnderdampedLangevinProcess
 from .ULI_data import StochasticTrajectoryData
 from .ULI_projectors import TrajectoryProjectors
-from .ULI_inference import UnderdampedLangevinInference
 from .ULI_bases import *
+from .ULI_inference import UnderdampedLangevinInference
 from .ULI_plotting_toolkit import *
