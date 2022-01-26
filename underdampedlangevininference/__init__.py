3dac56c846af86462f4138007c78c4aa6f4c23b7
William Davis <38541020+williamjsdavis@users.noreply.github.com>
2022-01-26 12:51:43 -0800

    Remove export of ULI_bases

--- underdampedlangevininference/__init__.py
+++ underdampedlangevininference/__init__.py
@@ -1,6 +1,5 @@
 from .ULI_langevin import UnderdampedLangevinProcess
 from .ULI_data import StochasticTrajectoryData
 from .ULI_projectors import TrajectoryProjectors
-from ULI_bases import ULI_bases
 from .ULI_inference import UnderdampedLangevinInference
 from .ULI_plotting_toolkit import *
